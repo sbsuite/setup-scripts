1049f32b3c5d74332753bdd904b4a83e8ff04303
Michael Sevestre <michael.sevestre@gmail.com>
2016-10-19 07:22:59 -0400

    Add log output to script

--- pre-setup.ps1
+++ pre-setup.ps1
@@ -1,6 +1,7 @@
 $fragmentsPath = "$env:APPVEYOR_BUILD_FOLDER\setup\$env:SETUP_PROJECT_NAME\fragments"
 $fragments = Get-ChildItem $env:APPVEYOR_BUILD_FOLDER\packages -Filter *.wxs -Recurse | %{$_.FullName} 
-Write-Host "Copying ($($fragments.Length)) *.wxs files to: $($fragmentsPaths)"
+$count = @($fragments).count;
+Write-Host "Copying $($count) *.wxs files to: $($fragmentsPath)"
 foreach($fragment in $fragments)
 {
     Copy-Item $fragment  $fragmentsPath

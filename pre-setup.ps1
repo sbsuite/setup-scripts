b111f8a6c153efae25beccbc9b72f2de759d8133
Michael Sevestre <michael.sevestre@gmail.com>
2016-11-01 10:57:18 -0400

    Update setup scripts

--- pre-setup.ps1
+++
@@ -1,8 +0,0 @@
-$fragmentsPath = "$env:SETUP_PROJECT_PATH/fragments"
-$fragments = Get-ChildItem $env:APPVEYOR_BUILD_FOLDER/packages -Filter *.wxs -Recurse | %{$_.FullName} 
-$count = @($fragments).count;
-Write-Host "Copying $($count) *.wxs files to: $($fragmentsPath)" -ForegroundColor Green
-foreach($fragment in $fragments)
-{
-    Copy-Item $fragment  $fragmentsPath
-}
